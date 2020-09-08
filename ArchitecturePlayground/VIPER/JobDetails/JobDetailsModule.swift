8065f76ed8a61dc867cc34d39235e9613d709d75
Andrew Strauss <astrauss@servicetitan.com>
2020-09-08 07:53:42 -0700

    additional VIPER modules

--- ArchitecturePlayground/VIPER/JobDetails/JobDetailsModule.swift
+++ ArchitecturePlayground/VIPER/JobDetails/JobDetailsModule.swift
@@ -7,3 +7,100 @@
 //
 
 import Foundation
+
+enum JobDetailsModule: Module {
+    struct Dependencies {
+        let userRepository: UserSessionRepositoryProtocol
+        let jobsAPI: JobsAPIProtocol
+    }
+
+    typealias Interactor = JobDetailsInteractor
+    typealias Router = JobDetailsRouter
+
+    static func viewController(presenter: JobDetailsPresenter, entity: Void) -> UIViewController {
+        let viewModel = JobDetailsViewModel()
+        let view = JobDetailsVIPERView(viewModel: viewModel, presenter: presenter)
+        presenter.view = viewModel
+
+        return UIHostingController(rootView: view)
+    }
+}
+
+// MARK: View I/O
+
+protocol JobDetailsViewInput: ViewInput {
+}
+
+protocol JobDetailsViewResponder: ViewResponder {
+}
+
+// MARK: Interactor I/O
+
+protocol JobDetailsInteractorInput: InteractorInput {
+}
+
+protocol JobDetailsInteractorResponder: InteractorResponder {
+}
+
+// MARK: Router I/O
+
+protocol JobDetailsRouterInput: RouterInput {
+}
+
+
+import SwiftUI
+
+class JobDetailsViewModel: ObservableObject, JobDetailsViewInput {
+
+}
+
+struct JobDetailsVIPERView: View, ViewProtocol {
+    @ObservedObject var viewModel: JobDetailsViewModel
+    var presenter: JobDetailsViewResponder
+
+    var body: some View {
+        EmptyView()
+    }
+}
+
+final class JobDetailsPresenter: PresenterProtocol {
+    weak var view: JobDetailsViewInput!
+    var interactor: JobDetailsInteractorInput
+    var router: JobDetailsRouterInput
+
+    init(interactor: JobDetailsInteractorInput, router: JobDetailsRouterInput) {
+        self.interactor = interactor
+        self.router = router
+    }
+}
+
+extension JobDetailsPresenter: JobDetailsViewResponder { }
+extension JobDetailsPresenter: JobDetailsInteractorResponder { }
+
+protocol JobDetailsInteractorDependencies {
+
+}
+
+final class JobDetailsInteractor: InteractorProtocol, PresenterResponder {
+    weak var presenter: JobDetailsInteractorResponder!
+
+    init(entity: Void, dependencies: JobDetailsInteractorDependencies) {
+
+    }
+}
+
+extension JobDetailsInteractor: JobDetailsInteractorInput { }
+
+protocol JobDetailsRouterDependencies {
+
+}
+
+final class JobDetailsRouter: RouterProtocol {
+    weak var viewController: UIViewController!
+
+    init(dependencies: JobDetailsRouterDependencies) {
+
+    }
+}
+
+extension JobDetailsRouter: JobDetailsRouterInput { }
--- ArchitecturePlayground/VIPER/JobList/JobListModule.swift
+++ ArchitecturePlayground/VIPER/JobList/JobListModule.swift
@@ -7,3 +7,100 @@
 //
 
 import Foundation
+
+enum JobListModule: Module {
+    struct Dependencies {
+        let userRepository: UserSessionRepositoryProtocol
+        let jobsAPI: JobsAPIProtocol
+    }
+
+    typealias Interactor = JobListInteractor
+    typealias Router = JobListRouter
+
+    static func viewController(presenter: JobListPresenter, entity: Void) -> UIViewController {
+        let viewModel = JobListViewModel()
+        let view = JobListVIPERView(viewModel: viewModel, presenter: presenter)
+        presenter.view = viewModel
+
+        return UIHostingController(rootView: view)
+    }
+}
+
+// MARK: View I/O
+
+protocol JobListViewInput: ViewInput {
+}
+
+protocol JobListViewResponder: ViewResponder {
+}
+
+// MARK: Interactor I/O
+
+protocol JobListInteractorInput: InteractorInput {
+}
+
+protocol JobListInteractorResponder: InteractorResponder {
+}
+
+// MARK: Router I/O
+
+protocol JobListRouterInput: RouterInput {
+}
+
+
+import SwiftUI
+
+class JobListViewModel: ObservableObject, JobListViewInput {
+
+}
+
+struct JobListVIPERView: View, ViewProtocol {
+    @ObservedObject var viewModel: JobListViewModel
+    var presenter: JobListViewResponder
+
+    var body: some View {
+        EmptyView()
+    }
+}
+
+final class JobListPresenter: PresenterProtocol {
+    weak var view: JobListViewInput!
+    var interactor: JobListInteractorInput
+    var router: JobListRouterInput
+
+    init(interactor: JobListInteractorInput, router: JobListRouterInput) {
+        self.interactor = interactor
+        self.router = router
+    }
+}
+
+extension JobListPresenter: JobListViewResponder { }
+extension JobListPresenter: JobListInteractorResponder { }
+
+protocol JobListInteractorDependencies {
+
+}
+
+final class JobListInteractor: InteractorProtocol, PresenterResponder {
+    weak var presenter: JobListInteractorResponder!
+
+    init(entity: Void, dependencies: JobListInteractorDependencies) {
+
+    }
+}
+
+extension JobListInteractor: JobListInteractorInput { }
+
+protocol JobListRouterDependencies {
+
+}
+
+final class JobListRouter: RouterProtocol {
+    weak var viewController: UIViewController!
+
+    init(dependencies: JobListRouterDependencies) {
+
+    }
+}
+
+extension JobListRouter: JobListRouterInput { }
